d95ee4685736b73ad17e961ae46b6c6babee024d
Daniel Truemper <truemped@googlemail.com>
2011-07-06 14:44:50 +0200

    [adisp] adding the library

---
+++ tornadotools/__init__.py
@@ -0,0 +1,28 @@
+#
+# Copyright (c) 2011 Daniel Truemper truemped@googlemail.com
+#
+# __init__.py 06-Jul-2011
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# under the License.
+#
+#
+"""
+.. _Tornado: http://www.tornadoweb.org
+.. _adisp: http://launchpad.net/adisp
+
+A set of tools for working with _Tornado.
+
+The first thing to be included is the _adisp library that helps in creating
+asynchronous applications.
+"""
