2db11003047c15dfefe6897cf047cf87a0c77be7
Daniel Truemper <truemped@googlemail.com>
2011-07-11 11:26:01 +0200

    adding examples to the documentation

--- tornadotools/mongrel2/__init__.py
+++ tornadotools/mongrel2/__init__.py
@@ -18,5 +18,64 @@
 #
 #
 """
-A Mongrel2 handler for Tornado.
+A Mongrel2 handler for Tornado based web apps.
+
+**Please note that this is considered EXPERIMENTAL!** Any feedback is very
+welcome! Add issues_ or fork the code at Github_!
+
+Usage is quite simple::
+
+    from tornadotools.mongrel2.handler import Mongrel2Handler
+    from zmq.eventloop.ioloop import IOLoop
+    from zmq.eventloop.ioloop import install as install_ioloop
+
+    # install the zmq version of the IOLoop
+    install_ioloop()
+
+    l = IOLoop.instance()
+
+    # create the handler with the `tornado.web.Application`
+    handler = Mongrel2Handler(app, "tcp://127.0.0.1:9999", "tcp://127.0.0.1:9998", io_loop=l)
+
+    # start listening for incoming messages from Mongrel2
+    handler.start()
+
+    # enter the ioloop
+    l.start()
+
+
+The Mongrel2 configuration could be as simple as this::
+
+    test_handler = Handler(
+        send_spec = "tcp://127.0.0.1:9999",
+        send_ident = "760deb97-b486-4e6a-b801-63fc01e93259",
+        recv_spec = "tcp://127.0.0.1:9998",
+        recv_ident = ""
+    )
+
+    test = Server(
+        uuid = "08eb25ae-50ef-43e2-bb3d-cf70437418fa",
+        name = "Test",
+
+        chroot = "./",
+        pid_file = "/run/dev.pid",
+        access_log = "/logs/dev-access.log",
+        error_log = "/logs/dev-error.log",
+
+        default_host = "localhost",
+        port = 6767,
+
+        hosts = [
+            Host(
+                name = "localhost",
+                routes={
+                    "/": test_handler
+                }
+            )
+        ]
+    )
+
+    servers = [test]
 """
+
+from .handler import Mongrel2Handler
