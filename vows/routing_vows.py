6b214280d8a4def11eb92c9397a93d80319a34ec
Daniel Truemper <truemped@googlemail.com>
2011-07-26 15:59:17 +0200

    initial vows for the routing decorator

---
+++ vows/routing_vows.py
@@ -0,0 +1,64 @@
+#
+# Copyright (c) 2011 Daniel Truemper truemped@googlemail.com
+#
+# routing_vows.py 26-Jul-2011
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+from tornado.web import Application, RequestHandler
+
+from tornadotools.route import Route
+
+from pyvows import Vows, expect
+from tornado_pyvows.context import TornadoContext, TornadoSubContext
+
+@Route(r"/test1")
+class TestHandler1(RequestHandler):
+    def get(self):
+        self.finish("Handler1")
+
+
+@Route(r"/test2", name="wohaa")
+class TestHandler2(RequestHandler):
+    def get(self):
+        self.finish("Handler2")
+
+
+@Vows.batch
+class RoutingDecorator(TornadoContext):
+
+    def _get_app(self):
+        return Application(Route.routes())
+
+    class RootBody(TornadoSubContext):
+
+        def topic(self):
+            return self._get("/")
+
+        def shouldNotBeFound(self, topic):
+            expect(topic.code).to_equal(404)
+
+    class Handler1(TornadoSubContext):
+
+        def topic(self):
+            return self._get("/test1")
+
+        def shouldNotBeAnError(self, topic):
+            expect(topic).not_to_be_an_error()
+
+        def hasHttpCode200(self, topic):
+            expect(topic.code).to_equal(200)
+
+        def bodyShouldBeHandler1(self, topic):
+            expect(topic.body).to_equal("Handler1")
